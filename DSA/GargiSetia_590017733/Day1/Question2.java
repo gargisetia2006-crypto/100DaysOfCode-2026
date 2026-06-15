f8648708f55e8e52571405e99d2a5be7f5143a87
Gargi Setia <gargisetia2006@gmail.com>
2026-06-15 12:40:03 +0530

    Day 1 Solutions

---
+++ DSA/GargiSetia_590017733/Day1/Question2.java
@@ -0,0 +1,13 @@
+
+class  Question2 {
+    public int  sum (int[] nums) {
+        int sum =0 ; 
+
+        for (int i =0 ; i<nums.length ; i++){
+         sum  =  nums[i]+sum  ; 
+        }
+
+return  sum  ; 
+    }
+    }
+
