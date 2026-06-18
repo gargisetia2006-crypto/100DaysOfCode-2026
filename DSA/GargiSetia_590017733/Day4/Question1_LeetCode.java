8c52ffa0bb6597784898f694902705cd8982bc6b
Gargi Setia <gargisetia2006@gmail.com>
2026-06-18 21:34:01 +0530

    Day 4 Solutions

---
+++ DSA/GargiSetia_590017733/Day4/Question1_LeetCode.java
@@ -0,0 +1,15 @@
+package DSA.GargiSetia_590017733.Day4;
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+
+
+        int ans[][] = new int[matrix[0].length][matrix.length];
+
+        for(int i =0 ; i < matrix.length ; i++){
+            for (int j =0 ; j < matrix [0].length ; j++){
+                         ans[j][i] = matrix[i][j];
+            }
+        }
+        return ans  ;
+    }
+}
