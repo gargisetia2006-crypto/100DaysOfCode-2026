1fffab14b0b516d20c068283817ef324151abb60
Gargi Setia <gargisetia2006@gmail.com>
2026-06-25 22:17:21 +0530

    Day 11 Solutions

---
+++ DSA/GargiSetia_590017733/Day11/Question2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Question2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = Character.toLowerCase(s.charAt(i));
+
+            if (ch != 'a' && ch != 'e' && ch != 'i' &&
+                ch != 'o' && ch != 'u' && ch != 'y') {
+
+                ans.append('.');
+                ans.append(ch);
+            }
+        }
+
+        System.out.println(ans);
+
+        sc.close();
+    }
+}
