8d8c34669dcdd03117d87c3eb41d385dbccc3a29
Gargi Setia <gargisetia2006@gmail.com>
2026-06-29 21:23:24 +0530

    Day 15 Solutions

---
+++ DSA/GargiSetia_590017733/Day15/Question2.java
@@ -0,0 +1,20 @@
+
+
+class Question2 {
+
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev;
+    }
+}
